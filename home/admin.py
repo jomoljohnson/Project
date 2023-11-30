a150d01840301a33fa7dcd3da85bf695fcdd11b5
jomoljohnson <jomolmariyajohnson2024b@mca.ajce.in>
2023-11-30 21:45:27 +0530

    files

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
-
-# Register your models here.
-
-
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username','email', 'user_type', 'is_active', 'date_joined')
+    list_filter = ('user_type', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username', 'user_type')}),
+        ('Permissions', {'fields': ('is_active', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'user_type'),
+        }),
+    )
+
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class SuperuserAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return User.objects.filter(is_superuser=False)
+    list_display = ('username','email', 'user_type', 'is_active', 'date_joined')
+
+# Register the custom admin class
+admin.site.register(User,SuperuserAdmin)
+
+#admin.site.register(CustomUser, CustomUserAdmin)
